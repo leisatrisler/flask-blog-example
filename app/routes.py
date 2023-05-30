b3a920bdf2dc67bb44b8bab0d67237cde079ecdc
Brian <bstanton773@gmail.com>
2023-05-30 14:43:58 -0500

    Render our first template!

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 def index():
     name = 'Brian'
     # name.append('Stanton')
-    return f'Hello {name}!!!!'
+    return render_template('index.html', first_name=name)
 
 @app.route('/test')
 def test():
