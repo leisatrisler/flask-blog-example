713dcb695f78353f9aa57521d915e752e57101c2
Brian <bstanton773@gmail.com>
2023-06-01 12:05:15 -0500

    Set up Flask-Migrate and create first migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
 
@@ -8,6 +9,8 @@ app.config.from_object(Config)
 
 # Create an instance of SQLAlchemy to connect our app to the database
 db = SQLAlchemy(app)
+# Create an instance of Migrate that will track our db and app
+migrate = Migrate(app, db)
 
 # import all of the routes from the routes file and models from the models file into the current package
 from app import routes, models
