52aa09f23c001e1fdfae7eb2676b1affd3e7c5bf
Brian <bstanton773@gmail.com>
2023-06-01 13:54:13 -0500

    Update signup route to add new users to database

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, db
 from flask import render_template, redirect, url_for, flash
 from app.forms import SignUpForm, LoginForm
+from app.models import User
 
 
 @app.route('/')
@@ -21,6 +22,17 @@ def signup():
         email = form.email.data
         password = form.password.data
         print(first_name, last_name, username, email, password)
+        # Query the user table to see if there are any users with that username or email
+        user_check = db.session.execute(db.select(User).where((User.username==username)|(User.email==email))).scalars().all()
+        # If there are any users with username or email, flash a warning message
+        if user_check:
+            flash('A user with that username and/or email already exists', 'danger')
+            return redirect(url_for('signup'))
+        # Create a new user
+        new_user = User(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+        # Add the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
         # flash a message saying user has signed up
         flash(f"{username} has signed up for the blog!", 'success')
         # Redirect back to the home page
